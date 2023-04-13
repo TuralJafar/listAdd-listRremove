d209ae8c9934793fdeadb7ee6296f5454db516b6
Shahvalad <shahvaladayvazov@gmail.com>
2023-04-13 13:24:19 +0400

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class MyList<T>
+        where T : class
+    {
+        T[] myArray;
+        public int Count { get; set; }
+        public void Add(T item)
+        {
+
+            if (Count == 0)
+            {
+                myArray = new T[4];
+                myArray[Count++] = item;
+            }
+            else if(Count >=myArray.Length) 
+            {
+                Array.Resize(ref myArray, myArray.Length*2);
+                myArray[Count++]=item;
+
+            }
+            else
+            {
+                myArray[Count++]=item;
+            }
+        }
+        public void Remove(T item)
+        {   int index=IndexOf(item);
+            if (index! == -1)
+            {
+                T[] newArray = new T[myArray.Length - 1];
+                int j = 0;
+                for (int i = 0; i < myArray.Length;)
+                {
+                    if (i != index)
+                    {
+                        newArray[j++] = myArray[i];
+                    }
+                }
+                myArray= newArray;
+                Count--;
+            }
+          
+        }
+        public int IndexOf(T item)
+        {
+          for(int i=0; i<myArray.Length; i++)
+            {
+                if (myArray[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            Console.WriteLine(list.Capacity);
+            list.Add("a");
+            list.Add("a");
+            list.Add("ab");
+            list.Add("a");
+            list.Add("a");
+            list.Remove("ab");
+            Console.WriteLine(list.Capacity);
+
+
+
+        }
+    }
+}
